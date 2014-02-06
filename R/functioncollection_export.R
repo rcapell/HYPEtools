a93e3e679b3a0d4aa89c1b0dffa9a4d3b72bae27
rcapell <rene.capell@gmail.com>
2014-02-06 20:04:24 +0100

    update with WriteBranchData

--- R/functioncollection_export.R
+++ R/functioncollection_export.R
@@ -4,7 +4,14 @@
 #     - WritePar()
 #     - WriteGeoData()
 #     - WriteGeoClass()
+#     - WriteBranchData()
 #     - 
+#
+#
+#   Internal helper functions:
+#
+#     - .CheckCharLengthDf()
+#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 
@@ -62,7 +69,8 @@ WritePar <- function (x, filename = "par.txt") {
 #'  
 #' @details
 #' \code{WriteGeoData} exports a GeoData dataframe with formatting options adjusted for the output to be read by HYPE.
-#' HYPE does not allow empty values in any GeoData column, the function will return with a warning if \code{NA}s were exported
+#' HYPE does neither allow empty values in any GeoData column nor any string elements with more than 50 characters, the 
+#' function will return with warnings if \code{NA}s or long strings were exported.
 #' (these are exported as "NA" to the text file).
 #' 
 #' @examples
@@ -77,6 +85,10 @@ WriteGeoData <- function(x, filename = "GeoData.txt") {
   if (!is.null(na.action(na.omit(x)))) {
     warning("NA values in exported object.")
   }
+  
+  # test length of string columns elements, throws warning if any element longer than 50
+  .CheckCharLengthDf(x, maxChar = 50)
+  
   # export
   write.table(x, file = filename, quote = FALSE, sep = "\t", row.names = FALSE)
   # reset options to defaults
@@ -105,7 +117,7 @@ WriteGeoData <- function(x, filename = "GeoData.txt") {
 #' Paths are separated by '/', not '\\'.
 #'  
 #' @details
-#' \code{WriteGeoData} exports a GeoClass dataframe with formatting options adjusted for the output to be read by HYPE.
+#' \code{WriteGeoClass} exports a GeoClass dataframe with formatting options adjusted for the output to be read by HYPE.
 #' HYPE accepts comment rows with a leading '!' in the beginning rows of a GeoClass file. The export function looks for those in 
 #' \code{attribute} 'comment', where \code{\link{ReadGeoClass}} stores such comments.
 #' 
@@ -138,3 +150,88 @@ WriteGeoClass <- function(x, filename = "GeoClass.txt") {
 # x <- gec
 # filename <- "GeoClass.txt"
 # rm(x, filename)
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~WriteBranchData~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#' @export
+#' @title
+#' Write a 'BranchData.txt' file
+#'
+#' @description
+#' This is a convenience wrapper function to export a 'BranchData.txt' file from R.
+#' 
+#' @param x The object to be written, a dataframe, as an object returned from \code{\link{ReadBranchData}}.
+#' @param filename A character string naming a file to write to. Windows users: Note that 
+#' Paths are separated by '/', not '\\'.
+#'  
+#' @details
+#' \code{WriteBranchData} exports a BranchData dataframe with formatting options adjusted for the output to be read by HYPE.
+#' HYPE does neither allow empty values in any BranchData column nor any string elements with more than 50 characters, the 
+#' function will return with warnings if \code{NA}s or long strings were exported.
+#' 
+#' @examples
+#' \dontrun{WriteBranchData(x = mybranchdata)}
+#' 
+
+
+WriteBranchData <- function(x, filename = "BranchData.txt") {
+  
+  # set options for number of digits and scientific notation so that HYPE-compatible decimal strings are returned
+  options(digits = 10, scipen = 5)
+  #
+  if (!is.null(na.action(na.omit(x)))) {
+    warning("NA values in exported object.")
+  }
+  
+  # test length of string columns elements, throws warning if any element longer than 50
+  .CheckCharLengthDf(x, maxChar = 50)
+  
+  # export
+  write.table(x, file = filename, quote = FALSE, sep = "\t", row.names = FALSE)
+  # reset options to defaults
+  options(digits = 7, scipen = 0)
+  
+}
+
+
+
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~.CheckCharLengthDf~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## internal function to test if string columns elements are longer than a number of characters
+# x: a dataframe to be tested
+# maxChar: maximum number of characters
+.CheckCharLengthDf <- function (x, maxChar) {
+  ## test if string columns elements are longer than 50 characters, which is the maximum accepted for BranchData by HYPE
+  
+  # which columns are of type factor or character (strings)
+  facts <- sapply(x, function(z) {is.factor(z)})
+  chars <- sapply(x, function(z) {is.character(z)})
+  lc <- length(which(chars))
+  lf <- length(which(facts))
+  
+  # select and convert factor columns to a character matrix, if factors exist
+  if (lf > 0) {
+    facmat <- as.matrix(sapply(x[, facts], as.character))
+    # test if the longest string in any column is longer than 50 characters, return with warning
+    if (max(apply(facmat, 2, function (z) max(nchar(z)))) > maxChar) {
+      warning("String with more than 50 characters in exported data detected. This will lead to an error in HYPE.")
+    }
+  }
+  # select and convert character columns to a character matrix, if characters exist
+  if (lc > 0) {
+    chamat <- as.matrix(x[, chars])
+    # test if the longest string in any column is longer than 50 characters, return with warning
+    if (max(apply(chamat, 2, function (z) max(nchar(z)))) > maxChar) {
+      warning("String with more than 50 characters in exported data detected. This will lead to an error in HYPE.")
+    }  
+  }
+}
