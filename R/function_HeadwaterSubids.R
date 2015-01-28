b2c5aea02f7f07716bc7e71e99bac56c31d20f16
rcapell <rene.capell@gmail.com>
2015-01-28 14:21:45 +0100

    new small function HeadwaterObs()

---
+++ R/function_HeadwaterSubids.R
@@ -0,0 +1,41 @@
+#' @export
+
+#' @title
+#' Find all headwater SUBIDs of a model domain
+#'
+#' @description
+#' Function to find all headwater SUBIDs of a HYPE model domain.
+#'
+#' @param gd A data frame, with two columns \code{subid} and \code{maindown}, (not case-sensitive). 
+#' Typically a 'GeoData.txt' file imported using \code{\link{ReadGeoData}}. 
+#' 
+#' @details
+#' \code{HeadwaterSubids} finds all headwater SUBIDs of a model domain as provided in a 'GeoData.txt' file, i.e. all subcatchments 
+#' which do not have any upstream subcatchments.
+#' 
+#' @return
+#' \code{HeadwaterSubids} returns a vector of outlet SUBIDs.
+#' 
+#' @seealso
+#' \code{\link{AllUpstreamSubids}}
+#' 
+#' @examples
+#' \dontrun{HeadwaterSubids(gd = mygeodata)}
+
+
+HeadwaterSubids <- function(gd) {
+  
+  # identify relevant columns
+  geocol.md <- which(tolower(colnames(gd)) == "maindown")
+  geocol.sub <- which(tolower(colnames(gd)) == "subid")
+  
+  # check existence
+  if (length(geocol.md) != 1 || length(geocol.sub) != 1) {
+    stop("SUBID and/or MAINDOWN column not found in 'gd'.")
+  }
+  
+  # select all subids which do not exist in maindown column, these are headwaters
+  res <- gd[!(gd[, geocol.sub] %in% gd[, geocol.md]), geocol.sub]
+  
+  return(res)
+}
