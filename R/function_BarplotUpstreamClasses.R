fd4af460b29e332674c7c23b193c08068898ff8a
rcapell <rene.capell@gmail.com>
2016-08-30 22:52:51 +0200

    First version BarplotUpstreamClasses(). Relates to issue #46.

--- R/function_BarplotUpstreamClasses.R
+++ R/function_BarplotUpstreamClasses.R
@@ -1,8 +1,8 @@
 
 #' Bar plots of upstream-averaged classes of HYPE sub-basins
 #' 
-#' Function to plot upstream-averaged landscape property classes of one or several sub-basins, e.g. land use or soils, 
-#' as bar plots.
+#' Function to plot upstream-averaged landscape property classes of one or several sub-basins as bar plots, e.g. 
+#' land use or soils.
 #' 
 #' @param x Data frame, containing column-wise class group fractions with SUBIDs in first column. Typically a result 
 #' from \code{\link{UpstreamGroupSLCClasses}}.
@@ -10,9 +10,9 @@
 #' \code{"soil"}, or \code{"crop"}, can be abbreviated.
 #' @param desc List for use with \code{type}. Class description labels imported from a 'description.txt' file, for bar labeling.
 #' See \code{\link{ReadDescription}} for formatting details.
-#' @param xlab Character string, x-axis label. Defaults to 
+#' @param xlab Character string, x-axis label, with defaults for standard groups land use, soil, and crops. 
 #' @param ylab Character string, y-axis label.
-#' @param ylim
+#' @param ylim Numeric, two element vector with limits for the y-axis. Defaults to values which give ample space for bar labels.
 #' @param class.names Character vector of class group names, with same length as number of class group fractions in \code{x}. 
 #' Specification of bar labels, alternative to arguments \code{type} and \code{desc}, in particular for cases where a non-standard
 #' grouping was used for \code{x}.
@@ -20,18 +20,34 @@
 #' arguments above).
 #' @param cex.axis Numeric, character expansion factor for axis annotation and labels.
 #' @param cex.names Numeric, character expansion factor for class group labels.
+#' @param col Colors for bars. Defaults to pre-defined color (sequences if multiple catchments). 
+#' @param legend.text Character, if provided, a legend will be plotted. Defaults to none if one sub-basin is plotted, and SUBIDs 
+#' if several sub-basins are plotted. Set to \code{NA} to prevent legend plotting in any case.
+#' @param legend.pos Character keyword for legend positioning, most likely \code{"left"} or \code{"right"}. For details, see 
+#' \code{\link{legend}}.
 #' @param pars List of tagged values which are passed to \code{\link{par}}.
 #' 
 #' @details 
-#' \code{BarplotUpstreamClasses} is a wrapper for \code{\link{barplot}}, with vertical labels plotted over the class group bars.
+#' \code{BarplotUpstreamClasses} is a wrapper for \code{\link{barplot}}, with vertical labels plotted over the class group bars. 
+#' Most arguments have sensible defaults, but can be adapted for fine-tuning if necessary.
+#' 
+#' @return 
+#' The function returns bar midpoints, see description in \code{\link{barplot}}.
 #' 
 #' @seealso 
 #' \code{\link{UpstreamGroupSLCClasses}}
 #' \code{\link{barplot}}
+#' 
+#' @examples 
+#' \dontrun{res <- UpstreamGroupSLCClasses(subid = 21, gd = mygeodata, gc = mygeoclass, bd = mybranchdata, type = "soil")}
+#' \dontrun{BarplotUpstreamClasses(x = res, type = "s", desc = mydescription)}
+#' 
+#' @export
 
 BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, ylab = "Area fraction (%)", 
                                     ylim = c(0,  max(x[, -1] * 1.5)), class.names = NULL, names.arg = rep("", ncol(x) - 1), 
-                                    cex.axis = 1, cex.names = .9, col = NULL, 
+                                    cex.axis = 1, cex.names = .9, col = NULL, legend.text = NULL, 
+                                    legend.pos = "left", 
                                     pars = list(mar = c(1.5, 3, .5, .5) + .1, mgp = c(1.5, .3, 0),  tcl = NA)) {
   # input argument checks
   if (!is.null(class.names) && ncol(x) -1 != length(class.names)) {
@@ -104,9 +120,26 @@ BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, yl
     }
   }
   
+  # legend text construction
+  if (is.null(legend.text)) {
+    if (nrow(x) == 1) {
+      leg.t <- NULL
+    } else {
+      leg.t <- x[, 1]
+    }
+  } else if (is.na(legend.text)) {
+    leg.t <- NULL
+  } else {
+    leg.t <- legend.text
+    if (length(leg.t) != nrow(x)) {
+      warning("Number of elements in 'legend.text' do not match number of rows in 'x'.")
+    }
+  }
+  
   # plot bars and labels
   res <- barplot(height = as.matrix(x[, -1]), names.arg = names.arg, beside = T, xlab = xlab, ylab = ylab, 
-                 ylim = ylim, border = NA, space = c(0, .2), col = col, cex.axis = cex.axis)
+                 ylim = ylim, border = NA, space = c(0, .2), col = col, cex.axis = cex.axis, legend.text = leg.t, 
+                 args.legend = list(border = NA, bty = "n", x = legend.pos, cex = cex.names))
   mtext(text = lgroup, side = 3, at = colMeans(res), line = -.1, padj = .5, cex = cex.names, las = 3, adj = 1)
   mtext(xlab, side = 1, line = .5, cex = cex.axis)
   box()
@@ -119,9 +152,7 @@ BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, yl
 # gd <- ReadGeoData("../PlotBasinSummary/GeoData.txt")
 # gcl <- ReadGeoClass("../PlotBasinSummary/GeoClass.txt", headrow = 4)
 # type <- "landuse"
-# x <- UpstreamGroupSLCClasses(subid = 8000152, gd = gd, gc = gcl, type = type)
 # x <- UpstreamGroupSLCClasses(subid = c(8000152, 8127943), gd = gd, gc = gcl, type = type)
-# desc <- NULL
 # desc <- ReadDescription("../PlotBasinSummary/description.txt")
 # pars = list(mar = c(1.5, 3, .5, .5) + .1, mgp = c(1.5, .3, 0),  tcl = NA)
 # ylab <- "Area fraction (%)"
@@ -132,3 +163,5 @@ BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, yl
 # class.names = NULL
 # xlab = NULL
 # col = NULL
+# legend.text = NULL
+# legend.pos <- "left"
