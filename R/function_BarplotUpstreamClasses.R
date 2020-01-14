2fa22bbeaec618a3df9dfaaab5cc7680e7152dab
rcapell <rene.capell@gmail.com>
2020-01-14 17:01:52 +0100

    Bug fix and improvements in BarplotUpstreamClasses.

--- R/function_BarplotUpstreamClasses.R
+++ R/function_BarplotUpstreamClasses.R
@@ -2,26 +2,27 @@
 #' Bar plots of upstream-averaged classes of HYPE sub-basins
 #' 
 #' Function to plot upstream-averaged landscape property classes of one or several sub-basins as bar plots, e.g. 
-#' land use or soils.
+#' land use or soils. Builds on \code{\link{barplot}}.
 #' 
 #' @param x Data frame, containing column-wise class group fractions with SUBIDs in first column. Typically a result 
 #' from \code{\link{UpstreamGroupSLCClasses}}. Column names of class group fractions _must_ end with \code{_x}, with x giving the
 #' class group ID, see details.
-#' @param type Character string keyword for use with \code{desc}. Type of class groups, either \code{"landuse"}, 
-#' \code{"soil"}, or \code{"crop"}, can be abbreviated.
-#' @param desc List for use with \code{type}. Class description labels imported from a 'description.txt' file, for bar labeling.
-#' See \code{\link{ReadDescription}} for formatting details.
-#' @param xlab Character string, x-axis label, with defaults for standard groups land use, soil, and crops. 
-#' @param ylab Character string, y-axis label.
-#' @param ylim Numeric, two element vector with limits for the y-axis. Defaults to values which give ample space for bar labels.
+#' @param type Character string keyword for class group labeling, used in combination with \code{desc}. Type of class groups, 
+#' either \code{"landuse"}, \code{"soil"}, or \code{"crop"} (abbreviations allowed). If \code{"custom"} (default),  labeling can be fine-
+#' tuned with argument \code{class.names}.
+#' @param desc List for use with \code{type}. Class description labels imported from a 'description.txt' file, for labeling of 
+#' standard class groups. See \code{\link{ReadDescription}} for formatting details.
 #' @param class.names Character vector of class group names, with same length as number of class group fractions in \code{x}. 
 #' Specification of bar labels, alternative to arguments \code{type} and \code{desc}, in particular for cases where a non-standard
 #' grouping was used for \code{x}.
+#' @param xlab Character string, x-axis label, with defaults for standard groups land use, soil, and crops. 
+#' @param ylab Character string, y-axis label.
+#' @param ylim Numeric, two element vector with limits for the y-axis. Defaults to values which give ample space for bar labels.
 #' @param names.arg Character vector, see \code{\link{barplot}}. Defaults to no labeling below bars (text labels within plot through 
 #' arguments above).
 #' @param cex.axis Numeric, character expansion factor for axis annotation and labels.
 #' @param cex.names Numeric, character expansion factor for class group labels.
-#' @param col Colors for bars. Defaults to pre-defined color (use vector of same length as number of SUBIDs if multiple catchments). 
+#' @param col Colors for bars. Defaults to \code{type}-specific pre-defined color. 
 #' @param legend.text Character, if provided, a legend will be plotted. Defaults to none if one sub-basin is plotted, and SUBIDs 
 #' if several sub-basins are plotted. Set to \code{NA} to prevent legend plotting in any case.
 #' @param legend.pos Character keyword for legend positioning, most likely \code{"left"} or \code{"right"}. For details, see 
@@ -51,85 +52,102 @@
 #' 
 #' @export
 
-BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, ylab = "Area fraction (%)", 
-                                    ylim = c(0,  max(x[, -1] * 150)), class.names = NULL, names.arg = rep("", ncol(x) - 1), 
-                                    cex.axis = 1, cex.names = .9, col = NULL, legend.text = NULL, 
-                                    legend.pos = "left", 
+BarplotUpstreamClasses <- function (x, type = c("custom", "landuse", "soil", "crop"), desc = NULL, class.names = NULL, xlab = NULL, 
+                                    ylab = "Area fraction (%)", ylim = c(-.05,  max(x[, -1] * 150)), names.arg = rep("", ncol(x) - 1), 
+                                    cex.axis = 1, cex.names = .9, col = NULL, legend.text = NULL, legend.pos = "left", 
                                     pars = list(mar = c(1.5, 3, .5, .5) + .1, mgp = c(1.5, .3, 0),  tcl = NA, xaxs = "i")) {
+  
   # input argument checks
+  
+  type <- match.arg(type)
+  
   if (!is.null(class.names) && ncol(x) -1 != length(class.names)) {
     stop("Number of classes in arguments 'x' and 'class.names' do not match.")
   }
   
-  # axis and bar label and color construction, conditional on function arguments
-  if (type == "landuse" || type == "l") {
-    # bar labels
-    if (!is.null(desc)) {
-      lgroup <- desc$lu
-      lid <- desc$lu.id
-    } else if (!is.null(class.names)) {
-      lgroup <- class.names
-      lid <- 1:length(class.names)
-    } else {
-      lgroup <- names(x)[-1]
-      lid <- sapply(strsplit(names(x)[-1], "_"), function(x) {as.numeric(x[2])})
+  
+  ## axis and bar label and color construction, conditional on function arguments
+  
+  if (type == "custom") {
+    # none of the standard types, desc ignored
+    if(!is.null(desc)) {
+      warning("Argument 'desc' ignored because argument 'type' is 'custom'.")
     }
-    # default value for x axis label
-    if (is.null(xlab)) {
-      xlab <- "Land use group"
-    }
-    # color for bars
-    if (is.null(col)) {
-      col <- "chartreuse3"
-    }
-    
-  } else if (type == "soil" || type == "s") {
-    if (!is.null(desc)) {
-      lgroup <- desc$so
-      lid <- desc$so.id
-    } else if (!is.null(class.names)) {
-      lgroup <- class.names
-      lid <- 1:length(class.names)
-    } else {
-      lgroup <- names(x[, -1])
-      lid <- sapply(strsplit(names(x)[-1], "_"), function(x) {as.numeric(x[2])})
-    }
-    if (is.null(xlab)) {
-      xlab <- "Soil group"
-    }
-    if (is.null(col)) {
-      col <- "indianred"
-    }
-    
-  } else if (type == "crop" || type == "c") {
-    if (!is.null(desc)) {
-      lgroup <- desc$cr
-      lid <- desc$cr.id
-    } else if (!is.null(class.names)) {
-      lgroup <- class.names
-      lid <- 1:length(class.names)
-    } else {
-      lgroup <- names(x[, -1])
-      lid <- sapply(strsplit(names(x)[-1], "_"), function(x) {as.numeric(x[2])})
-    }
-    if (is.null(xlab)) {
-      xlab <- "Crop group"
-    }
-    if (is.null(col)) {
-      col <- "plum3"
-    }
-    
-  } else {
-    if (!is.null(type)) {
-      warning(paste0("Unknown keyword ", type, " provided in argument 'type'."))
-    }
+    # class labels
     if (!is.null(class.names)) {
       lgroup <- class.names
     } else {
-      lgroup <- names(x[[, -1]])
+      lgroup <- names(x)[-1]
     }
+    # bar colour(s)
     if (is.null(col)) {
-      col <- ColBlues(nrow(x))
+      if (nrow(x) == 1) {
+        col <- "#9292D3"
+      } else {
+        col <- ColBlues(nrow(x))
+      }
+    }
+  } else {
+    # one of the three standard class types 
+    if (type == "landuse") {
+      # class labels
+      if (!is.null(desc)) {
+        lgroup <- desc$lu
+      } else if (!is.null(class.names)) {
+        lgroup <- class.names
+      } else {
+        lgroup <- names(x)[-1]
+      }
+      # default value for x axis label
+      if (is.null(xlab)) {
+        xlab <- "Land use group"
+      }
+      # color for bars
+      if (is.null(col)) {
+        if (nrow(x) == 1) {
+          col <- "chartreuse3"
+        } else {
+          col <- ColGreens(nrow(x))
+        }
+      }
+    } else if (type == "soil") {
+      if (!is.null(desc)) {
+        lgroup <- desc$so
+      } else if (!is.null(class.names)) {
+        lgroup <- class.names
+      } else {
+        lgroup <- names(x[, -1])
+      }
+      if (is.null(xlab)) {
+        xlab <- "Soil group"
+      }
+      if (is.null(col)) {
+        if (nrow(x) == 1) {
+          col <- "indianred"
+        } else {
+          col <- ColReds(nrow(x))
+        }
+      }
+      
+    } else if (type == "crop") {
+      if (!is.null(desc)) {
+        lgroup <- desc$cr
+      } else if (!is.null(class.names)) {
+        lgroup <- class.names
+      } else {
+        lgroup <- names(x[, -1])
+      }
+      if (is.null(xlab)) {
+        xlab <- "Crop group"
+      }
+      if (is.null(col)) {
+        col <- "plum3"
+        if (nrow(x) == 1) {
+          col <- "plum3"
+        } else {
+          col <- ColPurples(nrow(x))
+        }
+      }
     }
   }
   
@@ -149,16 +167,24 @@ BarplotUpstreamClasses <- function (x, type = NULL, desc = NULL, xlab = NULL, yl
     }
   }
   
+  
   # set plot parameters
   par(pars)
   
-  # get class IDs from column names in x
-  cid <- sapply(strsplit(names(x)[-1], "_"), function(x) {as.numeric(x[2])})
+  
+  # if group labels come from a description file, match class IDs with description labels 
+  # (individual classes can be missing in x)
+  if (!is.null(desc)) {
+    # get class IDs from column names in x
+    cid <- sapply(strsplit(names(x)[-1], "_"), function(x) {as.numeric(x[2])})
+    # select labels for existing groups existing in x
+    lgroup <- lgroup[cid]
+  }
   
   # plot bars and labels
   res <- barplot(height = as.matrix(x[, -1]) * 100, names.arg = names.arg, beside = T, xlab = xlab, ylab = ylab, 
                  ylim = ylim, border = NA, space = c(0, .2), col = col, cex.axis = cex.axis, legend.text = NULL)
-  mtext(text = lgroup[match(cid, lid)], side = 3, at = colMeans(res), line = -.2, padj = .3, cex = cex.names, las = 3, adj = 1)
+  mtext(text = lgroup, side = 3, at = colMeans(res), line = -.2, padj = .3, cex = cex.names, las = 3, adj = 1)
   mtext(xlab, side = 1, line = .5, cex = cex.names)
   abline(v = diff(colMeans(res))/2 + colMeans(res)[-ncol(res)] - .05, col = "grey90")
   if (!is.null(leg.t)) {
