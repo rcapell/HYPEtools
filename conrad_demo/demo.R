bd96583d196bb13f59f5513c60da0b5c97b53639
Brendel Conrad <a002416@ad.smhi.se>
2021-05-24 11:39:20 +0200

    Add plotly example

--- conrad_demo/demo.R
+++ conrad_demo/demo.R
@@ -1,11 +1,16 @@
 library(HYPEtools)
+
 library(leaflet)
+library(ggplot2)
+library(plotly)
 
 # Read HYPE Subbasin GIS Data
 hype_gis <- sf::st_read("//winfs-proj/data/proj/Fouh/Global/SouthAfrica/Model/GIS/gumhype_subbasins.shp")
 
 # Read MapOutput
-mapCOUT <- ReadMapOutput("mapCOUT.txt")
+mapCOUT <- ReadMapOutput("conrad_demo/mapCOUT.txt")
 
-# Launch Shiny App
+### I can edit/run app from the server.R and ui.R files in ./conrad_demo/ folder!! Maybe need to set path to conrad_demo folder?
+
+# Launch Shiny App - Need to build HYPEtools first!
 HYPEtools::launchApp(option.x = mapCOUT, option.map = hype_gis, option.var.name = "COUT")
