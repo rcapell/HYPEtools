87b10b93ebe3cff53f4451cd6f0c73d8f4316508
rene.capell <a001921@c21731.ad.smhi.se>
2021-06-23 20:00:06 +0200

    Version increment, to in-development.

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,5 @@
   library.dynam.unload("HYPEtools", libpath)
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("HYPEtools version 0.5.0. Find the latest release at: https://github.com/rcapell/HYPEtools/releases\nPlease report bugs and feature requests at: https://github.com/rcapell/HYPEtools/issues")
+  packageStartupMessage("HYPEtools version 0.5.0.9000. Find the latest release at: https://github.com/rcapell/HYPEtools/releases\nPlease report bugs and feature requests at: https://github.com/rcapell/HYPEtools/issues")
 }
