ab4ca1614537f657aad267715ec21849a864bdb9
Brendel Conrad <a002416@ad.smhi.se>
2021-10-21 13:06:24 +0200

    Update to add new variables for VariableLookup()

--- R/function_VariableLookup.R
+++ R/function_VariableLookup.R
@@ -55,41 +55,41 @@ VariableSearch <- function(search, info = c("ID", "Name", "Unit", "Description",
 # # _____________________________________________________________________________________________________________________________________
 # # Create Internal Package Data #####
 # # _____________________________________________________________________________________________________________________________________
-#
+# 
 # # Import Packages
 # library(xml2)
 # library(rvest)
 # library(dplyr)
 # library(stringr)
-#
+# 
 # # _____________________________________________________________________________________________________________________________________
 # # 1) Scrape Data from Wiki #####
 # # _____________________________________________________________________________________________________________________________________
 # # Wiki URL
 # url <- "http://www.smhi.net/hype/wiki/doku.php?id=start:hype_file_reference:info.txt:variables"
-#
+# 
 # # Scrape HTML
 # html <- read_html(url)
-#
+# 
 # # Extract Tables
 # tables <- html%>%
 #   html_elements("table")%>%
 #   html_table()
-#
+# 
 # # Get Simulated and Observed Variables
-# INTERNAL.hype.vars.info <- full_join(tables[[1]]%>%filter(!is.na(.[,"#"])), # Simulated Variables, Remove Rows with NA Number
+# vars.info <- full_join(tables[[1]]%>%filter(!is.na(.[,"#"])), # Simulated Variables, Remove Rows with NA Number
 #                       tables[[2]])%>% # Observed Variables
 #   rename("ID"="Variable ID","Reference"="Reference area","Aggregation"="Agg.") # Rename Columns
-#
+# 
 # # _____________________________________________________________________________________________________________________________________
 # # 2) Create Function to Create Short Description #####
 # # _____________________________________________________________________________________________________________________________________
-#
+# 
 # desc <- function(string){
-#
+# 
 #   # Find substrings within parentheses
 #   string_par <- regmatches(string, gregexpr("(?=\\().*?(?<=\\))", string, perl=T))[[1]]
-#
+# 
 #   if(length(string_par)>0){ # If any substrings within parentheses
 #     string_par_s <- grepl(" ",string_par) # Identify if there are any spaces within the substrings in the parentheses
 #     if(any(string_par_s==T)){ # If any spaces within the substrings in the parentheses
@@ -102,48 +102,49 @@ VariableSearch <- function(search, info = c("ID", "Name", "Unit", "Description",
 #   }
 #   if(grepl(",",string_out)==T){ # If there is a comma in the output string
 #     string_out <- gsub(",.*","",string_out) # Only get output string before first comma
-#
+# 
 #   }
 #   tools::toTitleCase(gsub(" $","", string_out, perl=T)) # Remove Trailing Spaces and convert to title case
 # }
-#
+# 
 # # _____________________________________________________________________________________________________________________________________
 # # 3) Format Variables Data Frame #####
 # # _____________________________________________________________________________________________________________________________________
-#
+# 
 # # Add Variable Name
-# INTERNAL.hype.vars.info$Name<- unlist(lapply(INTERNAL.hype.vars.info$Description,function(X){desc(X)}))
-#
+# vars.info$Name<- unlist(lapply(vars.info$Description,function(X){desc(X)}))
+# 
 # # Format Units
-# INTERNAL.hype.vars.info[which(grepl("\\p{So}C",INTERNAL.hype.vars.info$Unit,perl=T)),"Unit"] <- "C" # Remove Degree Symbol from Temperatures
-# INTERNAL.hype.vars.info[which(INTERNAL.hype.vars.info$Unit=="106m3"),"Unit"] <- "10^6 m3" # Fix Volume Units
-# INTERNAL.hype.vars.info$Unit <- gsub("[\xb5]","micro ",INTERNAL.hype.vars.info$Unit) # Replace Mu (Hex Code &#xb5) with word Micro
-#
-#
-#
+# vars.info[which(grepl("\\p{So}C",vars.info$Unit,perl=T)),"Unit"] <- "C" # Remove Degree Symbol from Temperatures
+# vars.info[which(vars.info$Unit=="106m3"),"Unit"] <- "10^6 m3" # Fix Volume Units
+# vars.info$Unit <- gsub("[\xb5]","micro ",vars.info$Unit) # Replace Mu (Hex Code &#xb5) with word Micro
+# 
+# 
+# 
 # # _____________________________________________________________________________________________________________________________________
 # # 4) Make Lists of Variables into Unique Rows #####
 # # _____________________________________________________________________________________________________________________________________
-#
+# 
 # # Identify Rows with Lists of Variables
-# fix_rows <- which(grepl(",",INTERNAL.hype.vars.info$ID))
-#
+# fix_rows <- which(grepl(",",vars.info$ID))
+# 
 # for(row in rev(fix_rows)){
 #   # Get Variable Names
-#   row_vars <- strsplit(INTERNAL.hype.vars.info$ID[row],",")[[1]]
-#
+#   row_vars <- strsplit(vars.info$ID[row],",")[[1]]
+# 
 #   # Add Rows for each Variable
 #   for(var_name in rev(row_vars)){
-#     INTERNAL.hype.vars.info <- INTERNAL.hype.vars.info%>%
-#       add_row(INTERNAL.hype.vars.info[row,]%>%mutate("ID"=var_name),
+#     vars.info <- vars.info%>%
+#       add_row(vars.info[row,]%>%mutate("ID"=var_name),
 #               .after=row)
 #   }
-#
+# 
 #   # Remove Original Row
-#   INTERNAL.hype.vars.info <- INTERNAL.hype.vars.info%>%slice(-row)
-#
+#   vars.info <- vars.info%>%slice(-row)
+# 
 # }
-#
+# 
 # # Save Data to package sysdata
 # load("R/sysdata.rda") # Load existing data in R/sysdata.rda
-# save(INTERNAL.hype.vars.info,INTERNAL.hype.vars, file = "R/sysdata.rda")
+# INTERNAL.hype.vars.info <- vars.info # Update with new vars.info
+# save(INTERNAL.hype.vars.info,INTERNAL.hype.vars, file = "R/sysdata.rda") # Save data from environment to sysdata
