bdc9e957ca4288d9d7fcad5e42e4cfe607285bbd
Brendel Conrad <a002416@ad.smhi.se>
2022-08-16 09:34:00 +0200

    Add question to ask when running devtools::release()

--- R/zzz.R
+++ R/zzz.R
@@ -6,3 +6,10 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0("HYPEtools version ", utils::packageVersion("HYPEtools"), ". Find the latest release at: https://github.com/rcapell/HYPEtools/releases\nPlease report bugs and feature requests at: https://github.com/rcapell/HYPEtools/issues"))
 }
+
+# Questions to ask when running devtools::release()
+release_questions <- function() {
+  c(
+    "Have you updated the internal package data for the VariableLookup() function? Use the code at bottom of function_VariableLookup.R"
+  )
+}
