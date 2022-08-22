d8ad9f4b95ad7de5ef1f1199ca79b672b7e1eb6a
rcapell <rene.capell@gmail.com>
2022-08-22 12:04:44 +0200

    Small changes prior to CRAN upload.

--- R/function_PlotMapPoints.R
+++ R/function_PlotMapPoints.R
@@ -6,7 +6,7 @@
 #' @param x Information to plot, typically model performances from imported HYPE 'subassX.txt' files. Data frame object
 #' with two columns, first column containing SUBIDs and second column containing model results to plot. See details.
 #' @param sites A \code{SpatialPointsDataFrame} or \code{sf} object. Typically an imported outlet point vector point file. Import of vector points
-#' requires additional packages, e.g. \code{\link[rgdal:readOGR]{rgdal}}.
+#' requires additional packages, e.g. \code{\link[sf::st_read]{sf}}.
 #' @param sites.subid.column Integer, column index in the \code{sites} 'data' \code{\link{slot}} holding SUBIDs (sub-catchment IDs).
 #' @param bg A \code{SpatialPolygonsDataFrame} or \code{sf} object to plot in the background. Typically an imported sub-basin vector polygon file.
 #' For default maps with several background layers, use \code{add = TRUE} and plot background layer(s) first.
@@ -94,10 +94,10 @@
 
 #' @examples
 #' \dontrun{
-#' require(rgdal)
+#' require(sf)
 #' x11(width = 5, height = 8)
-#' mysites <- readOGR(dsn = "../gisdata", layer = "myHYPEoutlets")
-#' mybg <- readOGR(dsn = "../gisdata", layer = "myHYPEsubids")
+#' mysites <- st_read(dsn = "../gisdata", layer = "myHYPEoutlets")
+#' mybg <- st_read(dsn = "../gisdata", layer = "myHYPEsubids")
 #' PlotMappoints(x = mysubass, sites = mysites, map.subid.column = 2, bg = mybg)
 #' }
 #'
