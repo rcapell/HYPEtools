cdf927709c906fa1dd5d89b31845a211a376caab
Brendel Conrad <a002416@ad.smhi.se>
2022-10-11 16:04:41 +0200

    Add Johan Plots! Need to update examples still

---
+++ R/function_PlotPerformanceByAttribute.R
@@ -0,0 +1,289 @@
+#'
+#' Plot model performance by SUBID attributes
+#'
+#' Create scatterplots of model performance by SUBID attributes.
+#'
+#' @param subass Information to plot, typically model performances from imported HYPE 'subassX.txt' files. Data frame object
+#' with first column containing SUBIDs and additional columns containing model results to plot. See details.
+#' @param subass.column Column index of information in \code{subass} to plot on the y-axis of the output plots.
+#' @param groups Optional data frame object to specify groups of SUBIDs to plot separately. First column should contain SUBIDs and second column should contain group IDs.
+#' @param attributes Data frame object containing the subbasin attribute information to plot on the x-axis of the output plots. Typically a data frame created by \code{\link{SubidAttributeSummary}}
+#' @param join.type Specify how to join \code{subass} to \code{attributes}. Default "join" will perform a \code{\link{left_join}} in which the order of the SUBIDs does not need to match. Additional option "cbind"
+#' will perform a \code{\link{cbind}} in which the order of the SUBIDs needs to match; this can be helpful if you want to create plots where \code{subass} performance data is calculated according to a 
+#' grouping variable (e.g. month).
+#' @param groups.color.pal Vector containing colors to use when plotting groups. Only used if groups is not \code{NULL}.
+#' @param alpha Numeric value to set transparency of dots in output plots. Should be in the range 0-1.
+#' @param trendline Logical, if \code{TRUE}, then trendlines will be added to the output plots. Set to \code{FALSE} to hide trendlines. See \code{\link{geom_smooth}}.
+#' @param trendline.method Specify method used to create trendlines. See \code{\link{geom_smooth}}.
+#' @param trendline.formula Specify formula used to create trendlines. See \code{\link{geom_smooth}}.
+#' @param trendline.alpha Numeric value to set transparency of trendlines in output plots. Should be in the range 0-1.
+#' @param trendline.darken Numeric value to make the trendlines darker color shades of their corresponding scatterplot points. Should be in the range 1-100.
+#' @param xlimits Vector containing minimum and maximum values for the x-axis of the output plots. See \code{\link{scale_x_continuous}}.
+#' @param xbreaks Vector containing the break values used for the x-axis of the output plots. See \code{\link{scale_x_continuous}}.
+#' @param xlabels Vector containing the labels for each break value used for the x-axis of the output plots. See \code{\link{scale_x_continuous}}.
+#' @param ylimits Vector containing minimum and maximum values for the y-axis of the output plots. See \code{\link{scale_y_continuous}}.
+#' @param ybreaks Vector containing the break values used for the y-axis of the output plots. See \code{\link{scale_y_continuous}}.
+#' @param ylabels Vector containing the labels for each break value used for the y-axis of the output plots. See \code{\link{scale_y_continuous}}.
+#' @param xlab String containing the text to use for the x-axis title of the output plots. See \code{\link{xlab}}.
+#' @param ylab String containing the text to use for the y-axis title of the output plots. See \code{\link{ylab}}.
+#' @param ncol Integer, number of columns to use in the output arranged plot. See \code{\link{ggarrange}}.
+#' @param nrow Integer, number of rows to use in the output arranged plot. See \code{\link{ggarrange}}.
+#' @param align Specify how output plots should be arranged. See \code{\link{ggarrange}}.
+#' @param common.legend Specify if arranged plot should use a common legend. See \code{\link{ggarrange}}.
+#' @param legend.position Specify position of common legend for arragned plot. See \code{\link{ggarrange}}.
+#' @param summary.table Logical, if \code{TRUE}, then a table providing summary statistics will be included at the bottom of the output plot.
+#' @param filename String, filename used to save plot. File extension must be specified. See \code{\link{ggsave}}.
+#' @param width Numeric, specify width of output plot. See \code{\link{ggsave}}.
+#' @param height Numeric, specify height of output plot. See \code{\link{ggsave}}.
+#' @param units Specify units of \code{width} and \code{height}. See \code{\link{ggsave}}.
+#' @param dpi Specify resolution of output plot. See \code{\link{ggsave}}.
+#'
+#' @details
+#' \code{PlotPerformanceByAttribute} can be used to analyze model performance according to subbasin attributes. The function requires two primary inputs; Model performance
+#' information is contained in the \code{subass} input, and subbasin attribute information is contained in the \code{attributes} input. The \code{subass.column} argument controls
+#' which column of the \code{subass} data frame will be used as the y-coordinate of points. Plots will be generated for each column in the \code{attributes} data frame
+#' (except for the column named "SUBID") using the column values as the x-coordinate of the points.
+#' 
+#' A subbasin attribute summary table can be generated using \code{\link{SubidAttributeSummary}}, and additional columns can be joined to the data frame to add additional output plots.
+#'
+#' @return
+#' \code{PlotPerformanceByAttribute} returns a plot to the currently active plot device.
+#'
+#' @seealso
+#' \code{\link{ReadSubass}} for HYPE result import; \code{\link{SubidAttributeSummary}} for subbasin attribute summary
+
+#' @examples
+#' \donttest{
+#' # Import plot data and subbasin polygons
+#' require(sf)
+#' te1 <- ReadSubass(filename = system.file("demo_model",
+#' "results", "subass1.txt", package = "HYPEtools"))
+#' te2 <- st_read(dsn = system.file("demo_model",
+#' "gis", "Nytorp_station.gpkg", package = "HYPEtools"))
+#' te2$SUBID <- 3587 # add station SUBID to point
+#' te3 <- st_read(dsn = system.file("demo_model",
+#' "gis", "Nytorp_map.gpkg", package = "HYPEtools"))
+#' # plot NSE performance for discharge
+#' PlotMapPoints(x = te1[, 1:2], sites = te2, sites.subid.column = 3, bg = te3)
+#' }
+#' 
+#' @importFrom dplyr group_by sym left_join n rename select summarize
+#' @importFrom ggplot2 aes element_text geom_point geom_smooth ggplot ggsave guide_legend guides scale_color_manual scale_fill_discrete scale_fill_manual scale_x_continuous
+#' scale_y_continuous theme unit waiver xlab ylab
+#' @importFrom ggpubr colnames_style ggarrange ggtexttable tab_add_title tbody_style ttheme
+#' @importFrom grDevices colorRampPalette hcl
+#' @importFrom stats median
+#' @export
+
+PlotPerformanceByAttribute <- function(subass, subass.column = 2, groups = NULL, attributes, join.type = c("join", "cbind"), groups.color.pal = NULL, alpha = 0.4,
+                                       trendline = TRUE, trendline.method = "lm", trendline.formula = NULL, trendline.alpha = 0.5, trendline.darken = 15,
+                                       xlimits = c(NA, NA), ylimits = c(NA, NA), xbreaks = waiver(), ybreaks = waiver(), xlabels = waiver(), ylabels = waiver(), xlab = NULL, ylab = NULL,
+                                       ncol = NULL, nrow = NULL, align = "hv", common.legend = TRUE, legend.position = "bottom", summary.table = FALSE,
+                                       filename = NULL, width = NA, height = NA, units = c("in", "cm", "mm", "px"), dpi = 300) {
+
+  # Check join type
+  join.type <- match.arg(join.type)
+
+  # Check trendline.darken value
+  if (trendline.darken < 1) {
+    warning("trendline.darken set must be in range 1-100. Setting to 1")
+  } else if (trendline.darken > 100) {
+    warning("trendline.darken set must be in range 1-100. Setting to 100")
+  }
+
+  # Create dataframe to store plot data
+  plotdata <- subass
+
+  # Join subass data to groups if they are given
+  if (!is.null(groups)) {
+    plotdata <- left_join(plotdata, groups, by = "SUBID") %>% rename("Group" = colnames(groups)[2])
+  }
+
+  # Join subass data to attribute data
+  if (join.type == "join") {
+    plotdata <- left_join(plotdata, attributes, by = "SUBID")
+  } else if (join.type == "cbind") {
+    if (!nrow(plotdata) == nrow(attributes)) {
+      stop("ERROR: number of rows in subass does not match number of rows in attributes")
+    }
+    plotdata <- cbind(plotdata, attributes %>% select(-SUBID))
+  }
+
+  # Create vector to store plots
+  plots <- vector("list")
+
+  # Generate plots
+  for (col in colnames(attributes)[which(!colnames(attributes) == "SUBID")]) {
+
+    # Create plot
+    if (!is.null(groups)) {
+      plot <- ggplot(data = plotdata, aes(x = !!sym(col), y = !!sym(colnames(subass)[subass.column]))) +
+        geom_point(aes(fill = Group), alpha = alpha, shape = 21, color = "transparent")
+    } else {
+      plot <- ggplot(data = plotdata, aes(x = !!sym(col), y = !!sym(colnames(subass)[subass.column]))) +
+        geom_point(alpha = alpha)
+    }
+
+    # Add trendlines
+    if (trendline == TRUE) {
+      if (!is.null(groups)) {
+        plot <- plot + geom_smooth(aes(color = Group), method = trendline.method, formula = trendline.formula)
+      } else {
+        plot <- plot + geom_smooth(method = trendline.method, formula = trendline.formula)
+      }
+    }
+
+    # Add x-axis label
+    if (!is.null(xlab)) {
+      plot <- plot + xlab(xlab)
+    }
+
+    # Add y-axis label
+    if (!is.null(ylab)) {
+      plot <- plot + ylab(ylab)
+    }
+
+    # Format colors if color palette specified
+    if (!is.null(groups.color.pal)) {
+      plot <- plot +
+        scale_fill_manual(values = groups.color.pal, name = "Group") +
+        scale_color_manual(values = unlist(lapply(groups.color.pal, function(X) {
+          colorRampPalette(c(X, "black"))(100)[trendline.darken] # Add darker colors for trendlines
+        })), name = "Group") + 
+        guides(color = guide_legend(override.aes = list(color = groups.color.pal))) # Override colors in legend to be the original colors
+
+      # Format colors if no color palette specified
+    } else {
+
+      # Function to get ggplot colors
+      gg_color_hue <- function(n) {
+        hues <- seq(15, 375, length = n + 1)
+        hcl(h = hues, l = 65, c = 100)[1:n]
+      }
+
+      # Get colors for ggplot
+      gg_colors <- gg_color_hue(length(unique(groups[[2]])))
+
+      # Adjust colors
+      plot <- plot +
+        scale_fill_discrete(name = "Group") + # Assign name to pallette for points
+        scale_color_manual(values = unlist(lapply(gg_colors, function(X) {
+          colorRampPalette(c(X, "black"))(100)[trendline.darken] # Add darker colors for trendlines
+        })), name = "Group") + 
+        guides(color = guide_legend(override.aes = list(color = gg_colors))) # Override colors in legend to be the original colors
+    }
+
+    # Format plot
+    plot <- plot +
+      scale_x_continuous(limits = xlimits, breaks = xbreaks, labels = xlabels) +
+      scale_y_continuous(limits = ylimits, breaks = ybreaks, labels = ylabels) +
+      theme(axis.title = element_text(face = "bold"))
+
+    # Store plot in list
+    plots[[col]] <- plot
+  }
+
+  # Determine if ncol and nrow should be automatically calculated
+  if (is.null(ncol) & is.null(nrow)) {
+    override <- TRUE
+  } else if ((ncol * nrow) < length(plots)) {
+    warning("ncol * nrow is less than the number of generated plots. Overriding ncol and nrow values.", call. = FALSE)
+    override <- TRUE
+  } else {
+    override <- FALSE
+  }
+
+  # Calculate ncol and nrow automatically if not specified
+  if (override == TRUE) {
+    # For 4 or fewer plots then just use one row
+    if (length(plots) <= 4) {
+      nrow <- 1
+      ncol <- length(plots)
+
+      # Otherwise use a square layout
+    } else {
+      ncol <- ceiling(length(plots)^0.5)
+      nrow <- ceiling(length(plots)^0.5)
+    }
+  }
+
+  # Arrange plots
+  arrangeplot <- ggarrange(plotlist = plots, ncol = ncol, nrow = nrow, align = align, common.legend = common.legend, legend = legend.position)
+
+  # Add summary stats table
+  if (summary.table == TRUE) {
+
+    # Calculate Summary Stats
+    if (!is.null(groups)) {
+      table <- plotdata %>%
+        group_by(Group) %>%
+        summarize(
+          n = n(),
+          Mean = round(mean(!!sym(colnames(subass)[subass.column]), na.rm = T), 2),
+          Median = round(median(!!sym(colnames(subass)[subass.column]), na.rm = T), 2),
+          Min = round(min(!!sym(colnames(subass)[subass.column]), na.rm = T), 2),
+          Max = round(max(!!sym(colnames(subass)[subass.column]), na.rm = T), 2)
+        )
+    } else {
+      table <- plotdata %>%
+        summarize(
+          n = n(),
+          Mean = round(mean(!!sym(colnames(subass)[subass.column]), na.rm = T), 2),
+          Median = round(median(!!sym(colnames(subass)[subass.column]), na.rm = T), 2)
+        )
+    }
+
+    # Create Colored Table
+    if (is.null(groups)) { # No groups
+      table.p <- ggtexttable(table,
+        rows = NULL,
+        theme = ttheme(
+          colnames.style = colnames_style(color = "Black", fill = "grey"),
+          tbody.style = tbody_style(size = 8, color = "black", fill = "lightgray")
+        )
+      )
+    } else if (!is.null(groups.color.pal)) { # Custom colors
+      table.p <- ggtexttable(table,
+        rows = NULL,
+        theme = ttheme(
+          colnames.style = colnames_style(color = "Black", fill = "grey"),
+          padding = unit(c(2, 2), "mm"),
+          tbody.style = tbody_style(size = 8, color = "black", fill = groups.color.pal)
+        )
+      )
+    } else { # No color ramp specified
+      table.p <- ggtexttable(table,
+        rows = NULL,
+        theme = ttheme(
+          colnames.style = colnames_style(color = "Black", fill = "grey"),
+          tbody.style = tbody_style(size = 8, color = "black", fill = gg_colors)
+        )
+      )
+    }
+
+    # Add title to table
+    if (!is.null(ylab)) {
+      table.p <- table.p %>%
+        tab_add_title(text = ylab, face = "bold", padding = unit(c(1, -2), "mm"))
+    } else {
+      table.p <- table.p %>%
+        tab_add_title(text = colnames(subass)[subass.column], face = "bold", padding = unit(c(1, -2), "mm"))
+    }
+
+    # Arrange plots
+    arrangeplot <- ggarrange(arrangeplot, table.p, nrow = 2, heights = c(1, 0.4))
+  }
+
+  # Save plot
+  if (!is.null(filename)) {
+    ggsave(filename = filename, plot = arrangeplot, width = width, height = height, units = units, dpi = dpi, bg = "white")
+  }
+
+  # Return plot
+  return(arrangeplot)
+}
+
+# Alias Easter Egg
+#' @rdname PlotPerformanceByAttribute
+#' @export
+PlotJohan <- PlotPerformanceByAttribute
---
+++ R/function_SubidAttributeSummary.R
@@ -0,0 +1,138 @@
+#'
+#' Summarize subbasin attributes
+#'
+#' Prepare data frame containing summary of subbasin attributes.
+#'
+#' @param subids Vector containing SUBIDs of subbasins to summarize.
+#' @param gd Imported HYPE GeoData.txt file. See \code{\link{ReadGeoData}}.
+#' @param bd Imported HYPE BranchData.txt file. See \code{\link{ReadBranchData}}.
+#' @param gc Imported HYPE GeoClass.txt file. See \code{\link{ReadGeoClass}}.
+#' @param desc Optional, Imported HYPE Description file. If provided, then dataframe columns will be renamed using the short names in the description file. See \code{\link{ReadDescription}}.
+#' @param group Optional, Integer vector of same length as number of SLC classes in gd. Alternative grouping index specification to gcl + type for \code{\link{UpstreamGroupSLCClasses}}.
+#' @param signif.digits Optional, Integer specifying number of significant digits to round outputs to. Used by \code{\link{UpstreamGroupSLCClasses}} and \code{link{UpstreamGeoData}}.
+#' @param progbar Logical, display a progress bar while calculating summary information. Used by \code{\link{UpstreamGroupSLCClasses}} and \code{link{UpstreamGeoData}}.
+#' @param summarize.landuse Logical, specify whether or not subbasin upstream landuse fractions should be calculated.
+#' @param summarize.soil Logical, specify whether or not subbasin upstream soil fractions should be calculated.
+#' @param summarize.crop Logical, specify whether or not subbasin upstream crop fractions should be calculated.
+#' @param summarize.upstreamarea Logical, specify whether or not subbasin upstream area should be calculated.
+#' @param unweighted.gd.cols Vector, names of \code{gd} columns which should be joined to the output data frame without any additional processing.
+#' @param upstream.gd.cols Vector, specify column names of \code{gd} which should be summarized using \code{\link{UpstreamGeoData}}.
+#' @param olake.slc Integer, SLC class number representing outlet lake fractions. Used by \code{\link{UpstreamGeoData}}.
+#' @param bd.weight Logical, if set to TRUE, flow weights will be applied for areas upstream of stream bifurcations. See \code{\link{UpstreamGeoData}}.
+#' @param mapoutputs Vector, paths to mapoutput files that should be read by \code{\link{ReadMapOutput}} and joined to the output data frame.
+#' 
+#' @details
+#' \code{SubidAttributeSummary} can be used to create a data frame object containing subbasin attribute summary information. This data frame can then be used as the \code{attributes}
+#' input for \code{\link{PlotPerformanceByAttribute}}. The function can summarize subbasin upstream landuse, soil, and crop fractions using \code{\link{UpstreamGroupSLCClasses}}. In addition, the
+#' function can summarize upstream GeoData information using \code{\link{UpstreamGeoData}}. Finally, the function can join mapoutput and GeoData columns directly to the output data frame (i.e without further processing). 
+#'
+#' @return
+#' \code{SubidAttributeSummary} returns a data frame object containing subbasin attribute summary information.
+#'
+#' @seealso
+#' \code{\link{UpstreamGroupSLCClasses}}, \code{\link{UpstreamGeoData}}, \code{\link{ReadMapOutput}} for subbasin attribute summary functions; \code{\link{PlotPerformanceByAttribute}} for related plotting function.
+
+#' @examples
+#' \donttest{
+#' # Import plot data and subbasin polygons
+#' require(sf)
+#' te1 <- ReadSubass(filename = system.file("demo_model",
+#' "results", "subass1.txt", package = "HYPEtools"))
+#' te2 <- st_read(dsn = system.file("demo_model",
+#' "gis", "Nytorp_station.gpkg", package = "HYPEtools"))
+#' te2$SUBID <- 3587 # add station SUBID to point
+#' te3 <- st_read(dsn = system.file("demo_model",
+#' "gis", "Nytorp_map.gpkg", package = "HYPEtools"))
+#' # plot NSE performance for discharge
+#' PlotMapPoints(x = te1[, 1:2], sites = te2, sites.subid.column = 3, bg = te3)
+#' }
+#' 
+#' @importFrom dplyr filter all_of left_join rename_with select
+#' @importFrom data.table setnames
+#' @importFrom purrr reduce
+#' @export
+
+# Create Johan summary dataframe
+SubidAttributeSummary <- function(subids, gd, bd = NULL, gc = NULL, desc = NULL, group = NULL, signif.digits = NULL, progbar = FALSE,
+                                  summarize.landuse = TRUE, summarize.soil = TRUE, summarize.crop = TRUE,
+                                  summarize.upstreamarea = TRUE, unweighted.gd.cols = NULL, upstream.gd.cols = NULL, olake.slc = NULL, bd.weight = FALSE,
+                                  mapoutputs = NULL) {
+
+  # Create list to store data
+  data <- vector("list")
+
+  # Summarize SLC Fractions --------------------------------------------------------------------------------------------------------------------
+
+  # Create list to store types
+  type <- c()
+  if (summarize.landuse == TRUE) {
+    type <- append(type, "landuse")
+  }
+  if (summarize.soil == TRUE) {
+    type <- append(type, "soil")
+  }
+  if (summarize.crop == TRUE) {
+    type <- append(type, "crop")
+  }
+
+  # Summarize upstream SLC fractions
+  if (any(summarize.landuse, summarize.soil, summarize.crop)) {
+    slc <- UpstreamGroupSLCClasses(
+      subid = subids,
+      gd = gd, bd = bd, gcl = gc,
+      type = type, group = group,
+      signif.digits = signif.digits, progbar = progbar
+    )
+
+    # Rename columns if given a description file
+    if (!is.null(desc)) {
+      slc <- slc %>%
+        setnames(paste0("landuse_", desc$lu.id), paste0("Landuse ", desc$lu), skip_absent = TRUE) %>% # Rename landuse columns
+        setnames(paste0("soil_", desc$so.id), paste0("Soil ", desc$so), skip_absent = TRUE) %>% # Rename soil columns
+        setnames(paste0("crop_", desc$cr.id), paste0("Crop ", desc$cr), skip_absent = TRUE) # Rename crop columns
+    }
+
+    data[["slc"]] <- slc
+  }
+
+  # Summarize Upstream Area & Upstream GeoData Columns --------------------------------------------------------------------------------------------------------------------
+
+  if (summarize.upstreamarea == TRUE | !is.null(upstream.gd.cols)) {
+
+    # Summarize upstream area & GeoData columns
+    uparea <- UpstreamGeoData(subid = subids, gd = gd %>% select(SUBID, MAINDOWN, AREA, all_of(upstream.gd.cols)), bd = bd, olake.slc = olake.slc, bd.weight = bd.weight, signif.digits = signif.digits, progbar = progbar) %>%
+      select(-MAINDOWN)
+
+    # Remove upstream area column if summarize.upstreamarea is FALSE
+    if (summarize.upstreamarea == FALSE) {
+      uparea <- uparea %>% select(-UP_AREA)
+    }
+
+    data[["uparea"]] <- uparea
+  }
+
+  # Add mapoutputs --------------------------------------------------------------------------------------------------------------------
+
+  if (!is.null(mapoutputs)) {
+    for (mapoutput in mapoutputs) {
+      mapdf <- ReadMapOutput(mapoutput) %>%
+        filter(SUBID %in% subids)
+
+      data[[basename(mapoutput)]] <- mapdf %>%
+        rename_with(~ paste0("map_", attr(mapdf, "variable")), .cols = 2)
+    }
+  }
+
+  # Merge Data --------------------------------------------------------------------------------------------------------------------
+
+  data <- data %>% reduce(full_join, by = "SUBID")
+
+  # Add unweighted GeoData columns --------------------------------------------------------------------------------------------------------------------
+  if (!is.null(unweighted.gd.cols)) {
+    data <- data %>%
+      left_join(gd %>% select(SUBID, all_of(unweighted.gd.cols)), by = "SUBID")
+  }
+
+  # Return data frame
+  return(data)
+}
