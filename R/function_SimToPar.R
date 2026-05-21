c4857d98d4442f509f9de68d68986c4a84b536a9
Conrad Brendel <a002416@ad.smhi.se>
2026-05-21 07:19:10 +0200

    Add additional ignore columns to SimToPar()

--- R/function_SimToPar.R
+++ R/function_SimToPar.R
@@ -43,7 +43,7 @@ AllSimToPar <- function(simfile, row, par){
   
   # Allsim column names from: http://hype.smhi.net//wiki/doku.php?id=start:hype_file_reference:allsim.txt&s[]=jpop
   # Bestsim column names from: http://hype.smhi.net//wiki/doku.php?id=start:hype_file_reference:bestsims.txt
-  stat_cols  <- c("NO", "CRIT", "rr2", "sr2", "mr2", "rmae", "sre", "rre", "mre", "rra", "sra", "mra", "tau", "md2", "mda", "mrs", "mcc", "mdkg", "akg", "asckg", "mar", "mdnr", "mnw", "snr", "smb", "numrc", "nummc", "jpop", "igen", "iacc")
+  stat_cols  <- c("NO", "CRIT", "rr2", "sr2", "mr2", "rmae", "sre", "rre", "mre", "rra", "sra", "mra", "tau", "md2", "mda", "mrs", "mcc", "mdkg", "akg", "asckg", "mar", "mdnr", "mnw", "snr", "smb", "numrc", "nummc", "jpop", "igen", "iacc", "igens", "jpops")
   
   # Get parameters from allsims or bestsims file
   pars_df <- data.table::transpose(simfile[row,], keep.names = "par") %>% # Get row from simfile
